c60f23762ec546e7f42518315f40b5fbda4fc44b
raulavilesrodriguez <finbravilesr@gmail.com>
2024-03-18 17:01:22 -0500

    first commit

---
+++ app/src/main/java/com/example/coursesgrid/data/Datasource.kt
@@ -0,0 +1,11 @@
+package com.example.coursesgrid.data
+
+import com.example.coursesgrid.model.Topic
+
+class Datasource {
+    fun loadTopics(): List<Topic> {
+        return listOf<Topic>(
+
+        )
+    }
+}
