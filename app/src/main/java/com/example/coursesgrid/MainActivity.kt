840e3d4a13999baefc26b48e6b582d691a37aa74
raulavilesrodriguez <finbravilesr@gmail.com>
2024-03-19 16:49:52 -0500

    my final version RA

--- app/src/main/java/com/example/coursesgrid/MainActivity.kt
+++ app/src/main/java/com/example/coursesgrid/MainActivity.kt
@@ -3,13 +3,38 @@ package com.example.coursesgrid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.coursesgrid.data.DataSource
+import com.example.coursesgrid.model.Topic
 import com.example.coursesgrid.ui.theme.CoursesGridTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    CoursesApp()
                 }
             }
         }
@@ -30,17 +55,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CoursesApp() {
+    CoursesList(coursesList = DataSource.topics)
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CoursesGridTheme {
-        Greeting("Android")
+fun CourseCard(topic: Topic, modifier: Modifier = Modifier){
+    Card(modifier = modifier,
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = topic.imageResourceId),
+                contentDescription = stringResource(id = topic.stringResourceId),
+                modifier = modifier
+                    .height(68.dp)
+                    .width(68.dp)
+            )
+            Column {
+                Text(
+                    text = LocalContext.current.getString(topic.stringResourceId),
+                    modifier = modifier.padding(
+                        top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp
+                    ),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    modifier = modifier.padding(start = 16.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = modifier
+                    )
+                    Spacer(modifier = modifier.padding(8.dp))
+                    Text(
+                        text = topic.courses.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+
+            }
+        }
     }
+}
+
+@Composable
+fun CoursesList(coursesList: List<Topic>, modifier: Modifier = Modifier){
+    LazyVerticalGrid(
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        columns = GridCells.Fixed(2)
+    ){
+        items(coursesList){course ->
+            CourseCard(
+                topic = course
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CoursesPreview() {
+    CoursesApp()
 }
--- app/src/main/java/com/example/coursesgrid/model/Topic.kt
+++ app/src/main/java/com/example/coursesgrid/model/Topic.kt
@@ -5,6 +5,7 @@ import androidx.annotation.StringRes
 
 data class Topic (
     @StringRes val stringResourceId: Int,
+    val courses: Int,
     @DrawableRes val imageResourceId: Int
 )
 
